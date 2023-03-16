29b230a0c69b77cc19d711ba52785b31f00484ea
Ilyas-Ismail <ilyasismaiil@hotmail.com>
2023-03-16 16:07:40 -0400

    Resolved conflict

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 
 export default function Home() {
   return (
-<<<<<<< Updated upstream
-    <div>HOME</div>
-=======
     <div>
       <Hero
         bookSearched = {bookSearched}
@@ -17,6 +14,5 @@ export default function Home() {
       <div className="mx-auto mb-3 m-lg-2 searchTitle d-flex justify-content-center"><h2 class="display-5 mt-3 mb-5 text-center">Results for - {searchText}</h2></div>
       <ShowResults/>
     </div>
->>>>>>> Stashed changes
   )
 }
