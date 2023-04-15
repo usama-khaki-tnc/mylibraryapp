aeb7b12040051e4e7ea80f0ba30cf25313dbaff9
Vikram Singh <72527314+vikramcen@users.noreply.github.com>
2023-04-15 12:31:14 -0400

    Profile added

--- server/models/User.js
+++ server/models/User.js
@@ -1,52 +1,29 @@
-const { Schema, model } = require('mongoose')
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
 
-const bookModel = require('./Book').schema;
-const borrowModel = require('./Borrow').schema;
-
-const userModel = new Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    phone: String,
+    country: String,
+    address: String,
+    city: String,
+    state: String,
+    zipCode: Number,
+    role: {
+      type: String,
+      default: "user",
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must use a valid email address'],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  borrowed: [borrowModel],
-  searchHistory: [String],
-},
-{
-  toJSON: {
-    virtuals: true,
-  }
-}
-)
+  { timestamps: true }
+);
 
-userModel.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
-userModel.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
-const User = model('User', userModel);
+const User = model("User", userSchema);
 
 module.exports = User;
